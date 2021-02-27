c5351b5c2227881ed583dea37dab62328fc4ab73
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-02-27 14:43:49 +0800

    Update to include metrics

--- sg-house.py
+++ sg-house.py
@@ -6,7 +6,7 @@ import seaborn as sns
 house_data = pd.read_csv('hdb_resale_price_Aug2019.csv')
 print(house_data.describe())
 ''' DATASET DETAILS 
-Metrics used (coefficients of matrices):
+Criteria used (coefficients of matrices):
 floor_area_sqm
 age
 remaining_lease_years
@@ -14,10 +14,10 @@ remaining_lease_years
 Output:
 resale_price
 '''
-metrics = ['floor_area_sqm', 'age', 'remaining_lease_years']
+criteria = ['floor_area_sqm', 'age', 'remaining_lease_years']
 town = input('Choose your town: ')
 y = house_data[(house_data['town'] == (str(town)).upper())]['resale_price']
-X = house_data[(house_data['town'] == (str(town)).upper())][metrics]
+X = house_data[(house_data['town'] == (str(town)).upper())][criteria]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=20)
 
@@ -29,7 +29,7 @@ print('Coefficients: \n', lm.coef_)
 
 guess = []
 for i in range(3):
-  print('What is the ' + str(metrics[i]) + '?')
+  print('What is the ' + str(criteria[i]) + '?')
   x = float(input())
   guess.append(x)
 est = 0
@@ -38,6 +38,13 @@ for i in range(3):
 print('Your estimated house price is ' + str(est))
 
 predictions = lm.predict(X_test)
+
+from sklearn import metrics
+print()
+print('Mean Absolute Error: ', metrics.mean_absolute_error(y_test, predictions))
+print('Root Mean Squared Error: ', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+print('Explained Variance Score: ', metrics.explained_variance_score(y_test, predictions))
+
 plt.scatter(y_test,predictions)
 plt.xlabel('Y Test')
 plt.ylabel('Predicted Y')
